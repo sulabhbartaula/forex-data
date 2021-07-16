d5b63e3823d883c8cfebc788b7db6e6a7ee683a9
Sulabh Bartaula <sulabhbartaula@gmail.com>
2021-07-16 18:49:27 +1000

    added subscribeservice

--- src/main/java/com/sulabh/forexdata/service/SubscribeService.java
+++ src/main/java/com/sulabh/forexdata/service/SubscribeService.java
@@ -1,26 +1,18 @@
-package com.sulabh.forexdata.controller;
+package com.sulabh.forexdata.service;
 
 import com.sulabh.forexdata.model.Api;
 import com.sulabh.forexdata.model.CurrencyPair;
 import com.sulabh.forexdata.model.ForexExchangeRate;
-import com.sulabh.forexdata.service.ApiFetchService;
-import com.sulabh.forexdata.service.CreateFileService;
-import com.sulabh.forexdata.service.EmailService;
-import com.sulabh.forexdata.service.SubscribeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ExecutionException;
 
-@Controller
-@RequestMapping("/")
-public class HomeController {
+@Service
+public class SubscribeService {
 
     String fileContent;
     Api api;
@@ -28,7 +20,6 @@ public class HomeController {
     String token = "60dd2b06a55d03.18616562";
 
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd'_'HHmm");
-
     // a filename of obsval_YYYYMMDD_HHMM.csv e.g. obsval_20191015_0800.csv
     String fileName = "obsval_"+ LocalDateTime.now().format(formatter) + ".csv";
 
@@ -44,21 +35,21 @@ public class HomeController {
     @Autowired
     EmailService emailService;
 
-    @Autowired
-    SubscribeService subscribeService;
+    @Scheduled(cron = "0 0 19,20 ?  * *")
+    public void scheduleSendingUpdates() throws ExecutionException {
 
-    @GetMapping("/")
-    public String showHomePage(){
-        return "index";
-    }
-
-    @GetMapping("/start")
-    public String startSubscription(Model model){
-        try {
-            subscribeService.scheduleSendingUpdates();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        for(String cp : currencyPair.getCurrencyPair()){
+            api = new Api(baseApi,token,cp);
+            String localApi = api.generateFinalApi();
+            ForexExchangeRate forexExchangeRate = apiFetchService.fetchDataFromApi(localApi);
+            fileContent =
+                    fileContent + "\n"+ forexExchangeRate.getCode().substring(0,6).concat(",").concat(forexExchangeRate.getOpen());
         }
-        return "index";
+
+        createFileService.createFile(fileContent,fileName);
+
+        emailService.sendEmail("sulabhbartaula@gmail.com",fileName);
     }
+
+
 }
